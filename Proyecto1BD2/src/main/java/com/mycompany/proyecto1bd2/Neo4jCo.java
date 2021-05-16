e55f7d74710213aa9e7145fbb29442af0cd9c8a2
MarcelaRivera2000 <marci.eunicer@gmail.com>
2021-05-16 16:34:12 -0600

    Base de datos(Neo4j) conectada a java

---
+++ Proyecto1BD2/src/main/java/com/mycompany/proyecto1bd2/Neo4jCo.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1bd2;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+
+import static org.neo4j.driver.Values.parameters;
+
+/**
+ *
+ * @author HP implements AutoCloseable
+ */
+public class Neo4jCo implements AutoCloseable{
+
+    private final Driver driver;
+
+    public Neo4jCo( String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    public void printGreeting( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    Result result = tx.run( "CREATE (a:Greeting) " +
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)",
+                            parameters( "message", message ) );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    public static void main( String... args ) throws Exception
+    {
+        try ( Neo4jCo greeter = new Neo4jCo( "bolt://localhost:7687", "Admin", "1234" ) )
+        {
+            greeter.printGreeting( "hello, world" );
+        }
+        System.out.println("LLEGO");
+    }
+        
+}
+   
+
